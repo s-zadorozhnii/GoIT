f0af3bf43822b7c239cf01b925b507255f731039
Serhii Zadorozhnii <fiest-a@mail.ru>
2017-05-05 09:58:11 +0300

    Change

---
+++ src/HomeWork_1_1/HomeTask_01_01.java
@@ -0,0 +1,17 @@
+package HomeWork_1_1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Serhii Zadorozhnii on 25.04.2017.
+ */
+public class HomeTask_01_01 {
+    public static void main(String[] args) {
+
+        System.out.println("Enter Your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+
+    }
+}
